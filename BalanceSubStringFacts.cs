91d84113b8aa673568fd8cbd499ef97fafe1733a
Zhu Yu <hubeiyichangzy@hotmail.com>
2021-01-10 17:05:59 +0800

    Create BalanceSubStringFacts.cs

---
+++ BalanceSubStringFacts.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using DotNetFramework461Console.Algorithms.MicrosoftInterview;
+using Xunit;
+
+namespace DotNetCoreXUnitTest
+{
+    public class MicrosoftInterviewFacts
+    {
+
+        #region TestBalanceSubString
+
+        [Fact]
+        public void ShortestBalanceItemFact()
+        {
+            Assert.Equal("aaa", new String(new List<char>{'a', 'a', 'a'}.ToArray()));
+
+            var s = "aA";
+            var item = new BalanceItem('a', 0, 1);
+            Assert.Equal(2, item.Distance());
+            Assert.True(item.IsBalance(s));
+            Assert.True(item.IsShortedBalance());
+            Assert.True(item.IsUpperLowerSet());
+        }
+
+        [Fact]
+        public void BalanceItemFact()
+        {
+            var s = "abAB";
+            var item = new BalanceItem("ab", 0, 3);
+            Assert.Equal(4, item.Distance());
+            Assert.True(item.IsBalance(s));
+            Assert.False(item.IsShortedBalance());
+            Assert.True(item.IsUpperLowerSet());
+
+            item.AddLowerCase('c');
+            Assert.Equal("ABC", item.UpperCases);
+        }
+        
+        [Fact]
+        public void NotBalanceItemFact()
+        {
+            var s = "abA";
+            var item = new BalanceItem("a", 0, 2);
+            Assert.Equal(3, item.Distance());
+            Assert.False(item.IsBalance(s));
+            Assert.False(item.IsShortedBalance());
+            Assert.True(item.IsUpperLowerSet());
+        }
+
+        [Fact]
+        public void SubStringFacts()
+        {
+            // int a = 1;
+            // int b = -1;
+            //
+            // Assert.Equal(2, a-b);
+            // Assert.Equal(2, 1-(-1));
+            // Assert.Equal(-1, new BalanceSubString().SubString("Ab"));
+            // Assert.Equal(2, new BalanceSubString().SubString("Aa"));
+            // Assert.Equal(4, new BalanceSubString().SubString("AbaB"));
+            Assert.Equal(-1, new BalanceSubString().SubString("Aba"));
+            // Assert.Equal(2, new BalanceSubString().SubString("AaABAbA"));
+            // Assert.Equal(5, new BalanceSubString().SubString("azABaabza"));
+        }
+        
+        [Fact]
+        public void SubString1Facts()
+        {
+            // Assert.Equal(-1, new BalanceSubString().SubString1("Ab"));
+            // Assert.Equal(2, new BalanceSubString().SubString1("Aa"));
+            // Assert.Equal(4, new BalanceSubString().SubString1("AbaB"));
+            Assert.Equal(-1, new BalanceSubString().SubString1("AbacB")); // case to check if you check IsBalanceString or not
+            // Assert.Equal(2, new BalanceSubString().SubString1("AaABAbA"));
+            // Assert.Equal(5, new BalanceSubString().SubString1("azABaabza"));
+        }
+        #endregion
+
+    }
+}
